74c6fc3f21c017d29e16dc8fb9b647d7dbc78930
Arjun059 <80092032+Arjun059@users.noreply.github.com>
2024-11-26 14:37:33 +0530

    Update settings.py

--- blog_app/blog_app/settings.py
+++ blog_app/blog_app/settings.py
@@ -38,12 +38,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'myapp',
     'shop',
-    'hospital',
+    'hospital',   
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
